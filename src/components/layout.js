8d40a222d70bfffa42d63497d85df2c22c0a1435
ksyunnnn <is.syunsukekobashi@gmail.com>
2019-02-23 06:03:59 +0900

    グローバルレイアウトの修正

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,10 @@
 import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
 import 'semantic-ui-css/semantic.min.css'
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        {console.log(data)}
-        <main>{children}</main>
-      </>
-    )}
-  />
+  <main>{children}</main>
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
